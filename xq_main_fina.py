8cc7f7cf7fa01c38f2d4c9efd82769ae1f4a7262
Your Name <you@example.com>
2021-11-12 20:48:07 +0800

    update xq_fina

--- xq_main_fina.py
+++ xq_main_fina.py
@@ -202,7 +202,7 @@ if __name__ == '__main__':
     nowdate=nowdate-datetime.timedelta(int(para1))
     print("nowdate is %s"%(nowdate.strftime("%Y-%m-%d")))
 
-    #update_database_indicator()
+    update_database_indicator()
     update_database_income()
     update_database_balance()
     update_database_cashflow()
