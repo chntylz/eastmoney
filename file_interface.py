4c98a3f855ca085e960ff9e81ebf0e919caad9e5
Your Name <you@example.com>
2021-12-17 12:34:09 +0800

    delete print

--- file_interface.py
+++ file_interface.py
@@ -267,15 +267,15 @@ def string2timestamp(strValue):
     t = d.timetuple() 
     timeStamp = int(time.mktime(t)) 
     timeStamp = float(str(timeStamp) + str("%06d" % d.microsecond))/1000000 
-    print(timeStamp)
+    #print(timeStamp)
     return timeStamp 
   except ValueError as e: 
-    print(e)
+    #print(e)
     d = datetime.datetime.strptime(strValue, "%Y-%m-%d") 
     t = d.timetuple() 
     timeStamp = int(time.mktime(t)) 
     timeStamp = float(str(timeStamp) + str("%06d" % d.microsecond))/1000000 
-    print(timeStamp)
+    #print(timeStamp)
     return timeStamp 
 
 # 1440751417.283 --> '2015-08-28 16:43:37.283' 
