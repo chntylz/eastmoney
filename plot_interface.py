5220448d62279a212736d0c242b29f2e26ea408f
Your Name <you@example.com>
2023-11-17 14:53:05 +0800

    update plot_interface

--- plot_interface.py
+++ plot_interface.py
@@ -12,37 +12,59 @@ hdata=HData_eastmoney_day("usr","usr")
 
 nowdate=datetime.datetime.now().date()
 nowcode='300045'
-detail_info = hdata.get_data_from_hdata(stock_code=nowcode,end_date=nowdate.strftime("%Y-%m-%d"), limit=20)
-detail_info = detail_info.head(5)
+detail_info = hdata.get_data_from_hdata(stock_code=nowcode,end_date=nowdate.strftime("%Y-%m-%d"), limit=500)
+#detail_info = detail_info.head(5)
 
 holder_df = hdata_holder.get_data_from_hdata(stock_code = nowcode)
 holder_df = holder_df.sort_values('record_date', ascending=1)
 holder_df = holder_df.reset_index(drop=True)
-holder_df = holder_df.head(5)
+#holder_df = holder_df.head(5)
 
 
-#x = np.linspace(0,10)
-#y = np.linspace(0,10)
+aa=detail_info
+bb=holder_df
 
-x = detail_info['record_date'].to_numpy()
-y = detail_info['close'].to_numpy()
+df = aa.append(bb, ignore_index=True, sort=False)
+df = df.sort_values('record_date')
+df = df.reset_index(drop=True)
+df = df.fillna(0)
+df_len = len(df)
 
 
-print('x=%d, y=%d'  % (len(x), len(y)))
+i = 0
+tmp_c = 0
+tmp_h = 0
+while ( i < df_len):
+    if(df.close[i] == 0):  
+        #df.close[i] = tmp_c       
+        df.loc[i,'close'] = tmp_c
+    if(df.holder_num[i] == 0):  
+        #df.holder_num[i] = tmp_h
+        df.loc[i,'holder_num'] = tmp_h
+    if(df.close[i]):    
+        tmp_c = df.close[i]        
+    if(df.holder_num[i]):
+        tmp_h = df.holder_num[i]
+    i = i + 1
+
+
+
+x = df['record_date'].to_numpy()
+y = df['close'].to_numpy()
+z = df['holder_num'].to_numpy()
+z = z/10000.0
+
+print('len(x)=%d, len(y)=%d, len(z)=%d'  % (len(x), len(y), len(z)))
 print(x)
 print(y)
-xx = holder_df['record_date'].to_numpy()
-zz = holder_df['holder_num'].to_numpy()
-print('xx=%d, zz=%d'  % (len(xx), len(zz)))
-print(xx)
-print(zz)
+print(z)
 
-fig = plt.figure()
+fig = plt.figure(figsize=(24, 30),dpi=240)
 ax = fig.add_subplot(111)
 ax.plot(x,y, '-', label = 'Quantity 1')
 
 ax2 = ax.twinx()
-ax2.plot(xx,zz, '-r', label = 'Quantity 2')
+ax2.plot(x,z, '-r', label = 'Quantity 2')
 fig.legend(loc=1, bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
 
 ax.set_xlabel("x [units]")
