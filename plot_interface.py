21bc0e598896c2f104333659dc4a92d62136b05c
Your Name <you@example.com>
2024-10-11 15:26:52 +0800

    update plot_interface

--- plot_interface.py
+++ plot_interface.py
@@ -5,14 +5,15 @@ import matplotlib.pyplot as plt
 
 import datetime as datetime
 from HData_eastmoney_day import *
-from HData_xq_holder import *
+from HData_eastmoney_holder import *
 
-hdata_holder=HData_xq_holder("usr","usr")
+hdata_holder=HData_eastmoney_holder("usr","usr")
 hdata=HData_eastmoney_day("usr","usr")
 
 nowdate=datetime.datetime.now().date()
 nowcode='300045'
-detail_info = hdata.get_data_from_hdata(stock_code=nowcode,end_date=nowdate.strftime("%Y-%m-%d"), limit=500)
+detail_info = hdata.get_data_from_hdata(stock_code=nowcode,
+        end_date=nowdate.strftime("%Y-%m-%d"), limit=500)
 #detail_info = detail_info.head(5)
 
 holder_df = hdata_holder.get_data_from_hdata(stock_code = nowcode)
@@ -34,17 +35,37 @@ df_len = len(df)
 i = 0
 tmp_c = 0
 tmp_h = 0
+
+#find the first valid data
+while ( i < df_len):
+    if(df.close[i]):  
+        tmp_c = df.close[i]
+        break
+    i = i + 1
+
+i = 0
+while ( i < df_len):
+    if(df.holder_num[i]):
+        tmp_h = df.holder_num[i]
+        break
+    i = i + 1
+
+#set 0 with valid data
 while ( i < df_len):
     if(df.close[i] == 0):  
         #df.close[i] = tmp_c       
         df.loc[i,'close'] = tmp_c
+
     if(df.holder_num[i] == 0):  
         #df.holder_num[i] = tmp_h
         df.loc[i,'holder_num'] = tmp_h
+
     if(df.close[i]):    
         tmp_c = df.close[i]        
+
     if(df.holder_num[i]):
         tmp_h = df.holder_num[i]
+
     i = i + 1
 
 
@@ -61,15 +82,19 @@ print(z)
 
 fig = plt.figure(figsize=(24, 30),dpi=240)
 ax = fig.add_subplot(111)
-ax.plot(x,y, '-', label = 'Quantity 1')
+ax.plot(x,y, '-', label = 'close')
+
+ax.set_xticks(range(0, len(df.index), 20))
+#ax.set_xticklabels(df.index[::20],rotation=45)
+ax.set_xticklabels(df['record_date'][::20],rotation=45)
 
 ax2 = ax.twinx()
-ax2.plot(x,z, '-r', label = 'Quantity 2')
+ax2.plot(x,z, '-r', label = 'holder_num')
 fig.legend(loc=1, bbox_to_anchor=(1,1), bbox_transform=ax.transAxes)
 
-ax.set_xlabel("x [units]")
-ax.set_ylabel(r"Quantity 1")
-ax2.set_ylabel(r"Quantity 2")
+ax.set_xlabel(" %s x [units]" % nowcode)
+ax.set_ylabel(r"close")
+ax2.set_ylabel(r"holder_num(w)")
 
 plt.savefig('0.png')
 
