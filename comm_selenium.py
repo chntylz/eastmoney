46737a69aa296fec119834913902383973766dd2
chntylz <chntylz@gmail.com>
2024-11-01 14:37:46 +0800

    optimize chromium parameter, memory

--- comm_selenium.py
+++ comm_selenium.py
@@ -33,7 +33,9 @@ def get_broswer():
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument("blink-settings=imagesEnabled=false")  #image disable
      
-    chrome_options.add_argument('--disable-dev-shm-usage')
+    #chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--disk-cache-dir=/dev/shm  --disk-cache-size=4096000000')
+
 
     try:
         browser = webdriver.Chrome(executable_path='/usr/bin/chromedriver',
@@ -66,3 +68,34 @@ def get_broswer():
     
     
 
+'''
+options.add_argument(‘headless’) # 无头模式
+options.add_argument(‘window-size={}x{}’.format(width, height)) # 直接配置大小和set_window_size一样
+options.add_argument(‘disable-gpu’) # 禁用GPU加速
+options.add_argument(‘proxy-server={}’.format(self.proxy_server)) # 配置代理
+options.add_argument(’–no-sandbox’) # 沙盒模式运行
+options.add_argument(’–disable-setuid-sandbox’) # 禁用沙盒
+options.add_argument(’–disable-dev-shm-usage’) # 大量渲染时候写入/tmp而非/dev/shm
+options.add_argument(’–user-data-dir={profile_path}’.format(profile_path)) # 用户数据存入指定文件
+options.add_argument('no-default-browser-check) # 不做浏览器默认检查
+options.add_argument("–disable-popup-blocking") # 允许弹窗
+options.add_argument("–disable-extensions") # 禁用扩展
+options.add_argument("–ignore-certificate-errors") # 忽略不信任证书
+options.add_argument("–no-first-run") # 初始化时为空白页面
+options.add_argument(’–start-maximized’) # 最大化启动
+options.add_argument(’–disable-notifications’) # 禁用通知警告
+options.add_argument(’–enable-automation’) # 通知(通知用户其浏览器正由自动化测试控制)
+options.add_argument(’–disable-xss-auditor’) # 禁止xss防护
+options.add_argument(’–disable-web-security’) # 关闭安全策略
+options.add_argument(’–allow-running-insecure-content’) # 允许运行不安全的内容
+options.add_argument(’–disable-webgl’) # 禁用webgl
+options.add_argument(’–homedir={}’) # 指定主目录存放位置
+options.add_argument(’–disk-cache-dir={临时文件目录}’) # 指定临时文件目录
+options.add_argument(‘disable-cache’) # 禁用缓存
+options.add_argument(‘excludeSwitches’, [‘enable-automation’]) # 开发者模式
+————————————————
+
+                            版权声明：本文为博主原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接和本声明。
+                        
+原文链接：https://blog.csdn.net/weixin_44929594/article/details/122513925
+'''
