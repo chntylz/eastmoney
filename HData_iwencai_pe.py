f6a3d8043f0dbd89ce29f4be159d8e0ea1a1ba2d
chntylz <chntylz@gmail.com>
2025-07-08 09:55:04 +0800

    add files

---
+++ HData_iwencai_pe.py
@@ -0,0 +1,462 @@
+#!/usr/bin/env python  
+# -*- coding: utf-8 -*-
+
+from file_interface import *
+import psycopg2
+import pandas as pd
+import time
+
+import os
+import numpy as np
+from io import StringIO
+
+
+debug = 0
+#debug = 1
+
+iwencai_cols = " run_date, stock_code, record_date, pe_pct " 
+
+
+class HData_iwencai_pe(object):
+    def __init__(self,user,password):
+        # self.aaa = aaa
+        self.iwencai_pe_table=[]
+        self.user=user
+        self.password=password
+
+        self.conn=None
+        self.cur=None
+
+    
+    def db_connect(self):
+        self.conn = psycopg2.connect(database="usr", \
+                                user=self.user, password=self.password, host="127.0.0.1",\
+                                port="5432")
+        self.cur = self.conn.cursor()
+
+    def db_disconnect(self):
+
+        self.conn.close()
+
+    def table_is_exist(self):
+        self.db_connect()
+        self.cur.execute("select count(*) from pg_class where relname = 'iwencai_pe_table' ;")
+        ans=self.cur.fetchall()
+        #my_dbg(list(ans[0])[0])
+        if list(ans[0])[0]:
+            self.conn.commit()
+            self.db_disconnect()
+            return True
+        else:
+            self.conn.commit()
+            self.db_disconnect()
+            return False
+
+        pass
+
+    def db_hdata_iwencai_create(self):
+
+        self.db_connect()
+
+        # 创建stocks表
+        self.cur.execute('''
+            drop table if exists iwencai_pe_table;
+            create table iwencai_pe_table(
+                run_date date,
+                stock_code varchar,
+                record_date date,
+                pe_pct float
+            );
+        alter table iwencai_pe_table add primary key(stock_code,record_date);
+            ''')
+        self.conn.commit()
+        self.db_disconnect()
+
+        my_dbg("db_iwencai_pe_table_create finish")
+        pass
+
+    def copy_from_stringio(self, df):
+        """
+        Here we are going save the dataframe in memory
+        and use copy_from() to copy it to the table
+        """
+        # save dataframe to an in memory buffer
+        buffer = StringIO()
+        #df.to_csv(buffer, index_label='id', header=False)
+        df.to_csv(buffer, index=0, header=False)
+        buffer.seek(0)
+
+        self.db_connect()
+        try:
+            self.cur.copy_from(buffer, table='iwencai_pe_table', sep=",")
+            self.conn.commit()
+        except (Exception, psycopg2.DatabaseError) as error:
+            my_dbg("Error: %s" % error)
+            self.conn.rollback()
+            self.db_disconnect()
+            return 1
+        
+        #my_dbg("copy_from_stringio() done")
+        self.db_disconnect()
+
+
+
+    def db_get_maxdate_of_stock(self,stock_code):#获取某支股票的最晚日期
+
+        self.db_connect()
+        self.cur.execute("select max(record_date) from iwencai_pe_table \
+                where stock_code=\'" + stock_code+ "\' ;")
+        ans=self.cur.fetchall()
+        if(len(ans)==0):
+            self.conn.commit()
+            self.db_disconnect()
+            return None
+        else:
+            self.conn.commit()
+            self.db_disconnect()
+            return ans[0][0]
+
+        pass
+
+    def insert_all_stock_data(self, data):
+        self.db_connect()
+        t1=time.time()
+
+        if debug:
+            my_dbg('insert_all_stock_data()')
+        if data is None:
+            my_dbg("None")
+        else:
+            length = len(data)
+            sql_cmd = ""
+            each_num = 1000
+            for i in range(0,length):
+                if debug:
+                    my_dbg (i)
+
+
+                str_temp= "\'" + str(data.iloc[i,0]) +  "\'"    #timestamp must be string
+                for j in range(1,data.shape[1]):
+                    str_temp+=",\'"+str(data.iloc[i,j]) + "\'"      #stock_code must be string
+
+                sql_cmd += "("+str_temp+")"
+                if i == 0:
+                    sql_cmd += ","
+                elif i % each_num == 0 or i == (length -1):
+                    pass
+                else:
+                    sql_cmd += ","
+
+                if i % each_num == 0 and i != 0:
+                    if debug:
+                        my_dbg(sql_cmd)
+                    if(sql_cmd != ""):
+                        final_cmd = "insert into iwencai_pe_table ("\
+                                + iwencai_cols + \
+                                " ) values "+sql_cmd+";"
+                        if debug:
+                            my_dbg(final_cmd)
+                        self.cur.execute(final_cmd)
+                        self.conn.commit()
+                        sql_cmd = ""
+
+            if debug:
+                my_dbg(sql_cmd)
+            if(sql_cmd != ""):
+                final_cmd = "insert into iwencai_pe_table ("\
+                        + iwencai_cols + \
+                        " ) values "+sql_cmd+";"
+                if debug:
+                    my_dbg(final_cmd)
+                self.cur.execute(final_cmd)
+                self.conn.commit()
+
+        if debug:
+            my_dbg(time.time()-t1)
+            my_dbg('insert_all_stock_data(\\)')
+
+        self.db_disconnect()
+
+    def insert_all_stock_data_2(self, data):
+        self.db_connect()
+        t0 = t1 = t2 = t3 = t4 = t5 = time.time()
+
+        if debug:
+            my_dbg('insert_all_stock_data()')
+
+        if data is None:
+            my_dbg("None")
+        else:
+            length = len(data)
+            sql_cmd = []
+            each_num = 1000
+            for i in range(length):
+                t1 = time.time()
+                if debug:
+                    my_dbg (i)
+
+
+                str_temp=[]
+                str_temp.append('\'')
+                str_temp.append(str(data.iloc[i,0]))
+                str_temp.append('\'')
+
+                #data.values.tolist()[0]
+                for j in range(1,data.shape[1]):
+                    str_temp.append(',\'')
+                    str_temp.append(str(data.iloc[i,j]))
+                    str_temp.append('\'')
+
+                sql_cmd.append('(')
+                sql_cmd.extend(str_temp)
+                sql_cmd.append(')')
+                if i == 0:
+                    sql_cmd.append(",")
+                elif i % each_num == 0 or i == (length -1):
+                    pass
+                else:
+                    sql_cmd.append(",")
+
+                if i % each_num == 0 and i != 0:
+                    if debug:
+                        my_dbg(sql_cmd)
+                        my_dbg("--------------------------------------")
+                        t=''.join(sql_cmd)
+                        my_dbg(t)
+                    if len(sql_cmd):
+                        final_sql = [] 
+                        final_sql.append("insert into iwencai_pe_table (")
+                        final_sql.append(iwencai_cols)
+                        final_sql.append( " ) values ")
+                        final_sql.append(''.join(sql_cmd))
+                        final_sql.append( " ; ")
+                        #my_dbg(''.join(final_sql))
+                        sql_cmd = []
+                        t2 = time.time()
+                        self.cur.execute(''.join(final_sql))
+                        t3 = time.time()
+                        self.conn.commit()
+                        t4 = time.time()
+                        my_dbg(t1, t2, t3, t4, t5)
+                t5 = time.time()
+                my_dbg(t5-t1)
+            if debug:
+                my_dbg(sql_cmd)
+                my_dbg(sql_cmd)
+                my_dbg("--------------------------------------")
+                t=''.join(sql_cmd)
+                my_dbg(t)
+
+            if len(sql_cmd):
+                final_sql = []
+                final_sql.append("insert into iwencai_pe_table (")
+                final_sql.append(iwencai_cols)
+                final_sql.append( " ) values ")
+                final_sql.append(''.join(sql_cmd))
+                final_sql.append( " ; ")
+                #my_dbg(''.join(final_sql))
+                sql_cmd = []
+                self.cur.execute(''.join(final_sql))
+                self.conn.commit()
+
+
+        if debug:
+            my_dbg(time.time()-t0)
+            my_dbg('insert_all_stock_data(\\)')
+
+
+        self.db_disconnect()
+
+
+    def insert_all_stock_data_3(self, data):
+        self.db_connect()
+        t0 = t1 = t2 = t3 = t4 = t5 = time.time()
+
+        if debug:
+            my_dbg('insert_all_stock_data_3()')
+
+        if data is None:
+            my_dbg("None")
+        else:
+            data.to_sql(name='iwencai_pe_table', con=self.conn, if_exists = 'replace', index=False)
+            pass
+
+        if debug:
+            my_dbg(time.time()-t0)
+            my_dbg('insert_all_stock_data_3(\\)')
+
+        self.db_disconnect()
+
+
+    #fix bug: delete zero when the stock is closed
+    def delete_amount_is_zero(self):
+        self.db_connect()
+        sql_temp="delete from iwencai_pe_table where amount = 0;"
+        self.cur.execute(sql_temp)
+
+        self.conn.commit()
+        self.db_disconnect()
+        pass
+
+
+    def get_data_from_hdata(self, stock_code=None, 
+                        start_date=None, 
+                        end_date=None,
+                        limit=0):#将数据库中的数据读取并转为dataframe格式返回
+        self.db_connect()
+        
+        and_flag = False
+
+
+        sql_temp = "select"  
+        sql_temp += iwencai_cols 
+        sql_temp += "from ( "
+
+        sql_temp += "select"
+        sql_temp += iwencai_cols
+        sql_temp += "from iwencai_pe_table"
+
+        if stock_code is None and start_date is None and end_date is None:
+            pass
+        else:
+            sql_temp += " where "
+
+        if stock_code is None:
+            pass
+        else:
+            sql_temp += " stock_code="+"\'"+stock_code+"\'"                       
+            and_flag |= True
+
+        if start_date is None:
+            pass
+        else:
+            if and_flag:
+                sql_temp += " and record_date >="+"\'"+start_date+"\'"                       
+            else:
+                sql_temp += " record_date >="+"\'"+start_date+"\'"                       
+            
+            and_flag |= True
+
+
+        if end_date is None:
+            pass
+        else:
+            if and_flag:
+                sql_temp += " and record_date <="+"\'"+end_date+"\'"                       
+            else:
+                sql_temp += " record_date <="+"\'"+end_date+"\'"                       
+
+
+        sql_temp += " order by record_date desc "                 
+
+        if limit == 0:
+            pass
+        else:
+            sql_temp += " LIMIT "+"\'"+str(limit)+"\'" 
+
+        sql_temp +=" ) as tbl order by record_date asc"
+
+        sql_temp += ";"
+
+        if debug:
+            my_dbg("get_data_from_hdata, sql_temp:%s" % sql_temp)
+
+
+
+        #select * from (select * from hdata_hsgt_table where stock_code='000922' order by record_date desc LIMIT 5) as tbl order by record_date asc;
+        self.cur.execute(sql_temp)
+        rows = self.cur.fetchall()
+
+        self.conn.commit()
+        self.db_disconnect()
+
+        dataframe_cols=[tuple[0] for tuple in self.cur.description]#列名和数据库列一致
+        df = pd.DataFrame(rows, columns=dataframe_cols)
+        df['record_date'] = df['record_date'].apply(lambda x: x.strftime('%Y-%m-%d'))        
+
+        if debug:
+            my_dbg(type(df))
+            my_dbg(df.head(2))
+    
+        return df
+        pass
+ 
+
+    def get_latest_data_from_hdata(self):#将数据库中的数据读取并转为dataframe格式返回
+
+        self.db_connect()
+        sql_temp = ' select * from iwencai_pe_table where record_date = '\
+                + '(select max(record_date) from iwencai_pe_table as tmp_date); '
+
+        if debug:
+            my_dbg("get_latest_data_from_hdata, sql_temp:%s" % sql_temp)
+
+        self.cur.execute(sql_temp)
+        rows = self.cur.fetchall()
+
+        self.conn.commit()
+        self.db_disconnect()
+
+        return rows
+
+        dataframe_cols=[tuple[0] for tuple in self.cur.description]#列名和数据库列一致
+        df = pd.DataFrame(rows, columns=dataframe_cols)
+        df['record_date'] = df['record_date'].apply(lambda x: x.strftime('%Y-%m-%d'))        
+
+        if debug:
+            my_dbg(type(df))
+            my_dbg(df.head(2))
+    
+        return df
+ 
+    def delete_data_from_hdata(self, stock_code=None, 
+                        start_date=None, 
+                        end_date=None,
+                        ):
+        self.db_connect()
+        
+        and_flag = False
+
+        sql_temp = "delete from iwencai_pe_table"
+
+        if stock_code is None and start_date is None and end_date is None:
+            self.db_disconnect()
+            pass
+            return
+        else:
+            sql_temp += " where "
+
+        if stock_code is None:
+            pass
+        else:
+            sql_temp += " stock_code="+"\'"+stock_code+"\'"                       
+            and_flag |= True
+
+        if start_date is None:
+            pass
+        else:
+            if and_flag:
+                sql_temp += " and record_date >="+"\'"+start_date+"\'"                       
+            else:
+                sql_temp += " record_date >="+"\'"+start_date+"\'"                       
+            
+            and_flag |= True
+
+
+        if end_date is None:
+            pass
+        else:
+            if and_flag:
+                sql_temp += " and record_date <="+"\'"+end_date+"\'"                       
+            else:
+                sql_temp += " record_date <="+"\'"+end_date+"\'"                       
+
+        sql_temp += ";"
+
+        my_dbg("delete_data_from_hdata, sql_temp:%s" % sql_temp)
+
+        self.cur.execute(sql_temp)
+        self.conn.commit()
+        self.db_disconnect()
+        pass
+ 
---
+++ eastmoney_slide.py
@@ -0,0 +1,216 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Created by aneasystone on 2018/3/2
+import random
+
+from comm_selenium import *
+
+from PIL import Image, ImageChops
+from numpy import array
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait as Wait
+from selenium.webdriver.support import expected_conditions as Expect
+import requests, io, re
+import easing
+import time
+
+'''
+https://www.aneasystone.com/archives/2018/03/python-selenium-geetest-crack.html
+
+输出的验证码为JSON格式，其中大图片是将原图裁剪成横向10份纵向2分共20张图片随机混淆拼接而成的，原图通过在前端移位还原，混淆信息带在JSON上
+https://github.com/geffzhang/Geetest
+
+
+破解极验三代滑动验证，成功率百分之百（二）：分析图片得到滑动距离
+https://zhuanlan.zhihu.com/p/569492595
+
+
+Python爬虫入门教程 58-100 python爬虫高级技术之验证码篇4-极验证识别技术之一
+https://developer.aliyun.com/article/705957
+
+
+
+https://github.com/1046517444/SliderCracker-1/commit/dabb91ea8b1783d1afb6e14f1df384e2786c1c4e#diff-99ad9999b01aa83906f30952b2c61bfb5e8207528a86f4b4cd2110ec967c0f51
+
+
+full
+https://smartvcode2.eastmoney.com/02/resources/e02b_160/2/1e/1e7e08b4ebe7e83ab59ec113256dc7b8/1e7e08b4ebe7e83ab59ec113256dc7b8.jpg
+
+slice
+https://smartvcode2.eastmoney.com/02/resources/e02b_160/2/1e/1e7e08b4ebe7e83ab59ec113256dc7b8/slice/2378b30a.png
+'''
+
+def convert_css_to_offset(px):
+    ps = px.replace('px', '').split(' ')
+    x = -int(ps[0])
+    y = -int(ps[1])
+    return x, y, x + 10, y + 58
+
+
+def convert_index_to_offset(index):
+    row = int(index / 26)
+    col = index % 26
+    x = col * 10
+    y = row * 58
+    return x, y, x + 10, y + 58
+
+
+def get_slider_offset_from_diff_image(diff):
+    im = array(diff)
+    width, height = diff.size
+    diff = []
+    for i in range(height):
+        for j in range(width):
+            # black is not only (0,0,0)
+            if im[i, j, 0] > 15 or im[i, j, 1] > 15 or im[i, j, 1] > 15:
+                diff.append(j)
+                break
+    return min(diff)
+
+
+def get_slider_offset(image_url, image_url_bg, css):
+    image_file = io.BytesIO(requests.get(image_url).content)
+    im = Image.open(image_file)
+    image_file_bg = io.BytesIO(requests.get(image_url_bg).content)
+    im_bg = Image.open(image_file_bg)
+    # im.show()
+    # im_bg.show()
+
+    # 10*58 26/row => background image size = 260*116
+    captcha = Image.new('RGB', (260, 116))
+    captcha_bg = Image.new('RGB', (260, 116))
+    for i, px in enumerate(css):
+        offset = convert_css_to_offset(px)
+        region = im.crop(offset)
+        region_bg = im_bg.crop(offset)
+        offset = convert_index_to_offset(i)
+        captcha.paste(region, offset)
+        captcha_bg.paste(region_bg, offset)
+    diff = ImageChops.difference(captcha, captcha_bg)
+    # captcha.show()
+    # captcha_bg.show()
+    # diff.show()
+    diff.save('./diff.png')
+    return get_slider_offset_from_diff_image(diff)
+
+
+def get_image_css(images):
+    css = []
+    for image in images:
+        position = get_image_position_from_style(image.get_attribute("style"))
+        css.append(position)
+    return css
+
+
+def get_image_url_from_style(style):
+    match = re.match('background-image: url\("(.*?)"\); background-position: (.*?);', style)
+    return match.group(1)
+
+
+def get_image_position_from_style(style):
+    match = re.match('background-image: url\("(.*?)"\); background-position: (.*?);', style)
+    return match.group(2)
+
+
+def get_slice_offset(slice):
+    style = slice.get_attribute("style")
+    match = re.search('background-image: url\("(.*?)"\);', style)
+    url = match.group(1)
+    image_file = io.BytesIO(requests.get(url).content)
+    im = Image.open(image_file)
+    im.save('./slice.png')
+    return get_slider_offset_from_diff_image(im)
+
+
+# refer: https://ask.hellobi.com/blog/cuiqingcai/9796
+def get_track(distance):
+    track = []
+    current = 0
+    mid = distance * 4 / 5
+    t = 0.2
+    v = 0
+
+    while current < distance:
+        if current < mid:
+            a = 2
+        else:
+            a = -3
+        v0 = v
+        v = v0 + a * t
+        move = v0 * t + 1 / 2 * a * t * t
+        current += move
+        track.append(round(move))
+    return track
+
+
+def fake_drag(browser, knob, offset):
+    # seconds = random.uniform(2, 6)
+    # print(seconds)
+    # samples = int(seconds*10)
+    # diffs = sorted(random.sample(range(0, offset), samples-1))
+    # diffs.insert(0, 0)
+    # diffs.append(offset)
+    # ActionChains(browser).click_and_hold(knob).perform()
+    # for i in range(samples):
+    #     ActionChains(browser).pause(seconds/samples).move_by_offset(diffs[i+1]-diffs[i], 0).perform()
+    # ActionChains(browser).release().perform()
+
+    # tracks = get_track(offset)
+    tracks = get_track(offset)
+    #offsets, tracks = easing.get_tracks(offset, 12, 'ease_out_expo')
+    #print(offsets)
+    ActionChains(browser).click_and_hold(knob).perform()
+    for x in tracks:
+        ActionChains(browser).move_by_offset(x, 0).perform()
+    ActionChains(browser).pause(0.5).release().perform()
+
+    return
+
+
+def do_crack(browser):
+    slice = browser.find_element_by_class_name("em_slice")
+    slice_offset = get_slice_offset(slice)
+    print(slice_offset)
+
+    images = browser.find_elements_by_class_name("em_cut_fullbg_slice")
+    images_bg = browser.find_elements_by_class_name("em_cut_bg_slice")
+    image_url = get_image_url_from_style(images[0].get_attribute("style"))
+    image_url_bg = get_image_url_from_style(images_bg[0].get_attribute("style"))
+    css = get_image_css(images)
+    offset = get_slider_offset(image_url, image_url_bg, css)
+    print(offset)
+
+    knob = browser.find_element_by_class_name("em_slider_knob")
+    # ActionChains(browser).drag_and_drop_by_offset(knob, offset - slice_offset, 0).perform()
+    try:
+        fake_drag(browser, knob, offset - slice_offset)
+        time.sleep(10)
+        fake_drag(browser, knob, offset - slice_offset)
+    except Exception as e:
+        my_dbg(e)
+        fake_drag(browser, knob, offset - slice_offset)
+    finally:
+        pass
+
+    return
+
+
+browser = get_browser(1)
+browser.refresh()
+#browser.switch_to.frame(11)
+# 等待iframe加载并切换到该iframe
+iframe = WebDriverWait(browser, 10).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "popwscps_d_iframe"))
+                )
+
+# 切换到iframe内部
+browser.switch_to.frame(iframe)
+time.sleep(3)
+
+browser.find_element('xpath', '//div[contains(@class, "em_slider_knob")]').click()
+my_dbg('test')
+do_crack(browser)
+
+
