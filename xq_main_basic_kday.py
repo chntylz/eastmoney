48fdd674c8ac20fb3e9883434a0c9493af354922
chntylz <chntylz@gmail.com>
2025-09-09 13:03:59 +0800

    fix xq bug

--- xq_main_basic_kday.py
+++ xq_main_basic_kday.py
@@ -15,7 +15,7 @@ debug = 0
 from HData_xq_simple_day import *
 from HData_xq_kday import *
 
-hdata_day=HData_xq_simple_day("usr","usr")
+hdata_s_day=HData_xq_simple_day("usr","usr")
 hdata_kday=HData_xq_kday("usr","usr")
 
 def check_kday_table():
@@ -29,13 +29,13 @@ def check_kday_table():
         my_dbg('kday table not exist, create')
 
 def check_day_table():
-    table_exist = hdata_day.table_is_exist() 
+    table_exist = hdata_s_day.table_is_exist() 
     my_dbg('table_exist=%d' % table_exist)
     if table_exist:
-        #hdata_day.db_hdata_xq_simple_create()
+        #hdata_s_day.db_hdata_xq_simple_create()
         my_dbg('kday table already exist')
     else:
-        hdata_day.db_hdata_xq_simple_create()
+        hdata_s_day.db_hdata_xq_simple_create()
         my_dbg('kday table not exist, create')
 
 
@@ -47,15 +47,20 @@ if __name__ == '__main__':
     t1 = time.time()
     df = xq_get_realtime_data()
     if len(df):
+        my_dbg(f'df columns: {df.columns}')
         df=df.fillna(0)
+        df = df.drop_duplicates(subset=['symbol'], keep='first')
         df.insert(0, 'record_date', nowdate.strftime("%Y-%m-%d"), allow_duplicates=False)
         df.to_csv('./csv/'+ datetime.datetime.now().strftime('%Y-%m-%d-%H-%M') + '_df_xq_simple'  +'.csv', encoding='gbk')
         check_day_table()
-        hdata_day.delete_data_from_hdata(
+        hdata_s_day.delete_data_from_hdata(
                 start_date=nowdate.strftime("%Y-%m-%d"),
                 end_date=nowdate.strftime("%Y-%m-%d")
                 )
-        hdata_day.copy_from_stringio(df)
+        try:
+            hdata_s_day.copy_from_stringio(df)
+        except Exception as e:
+            my_dbg(f"error:{e}")
     else:
         my_dbg('#ERROR df is null')
 
@@ -63,13 +68,20 @@ if __name__ == '__main__':
 
     kday_df = xq_get_kday_data()
     if len(kday_df):
+        my_dbg(f'kday_df columns: {kday_df.columns}')
+        kday_df = kday_df.drop_duplicates(subset=['symbol'], keep='first')
         kday_df.to_csv('./csv/'+ datetime.datetime.now().strftime('%Y-%m-%d-%H-%M') + '_df_xq_kday'  +'.csv', encoding='gbk')
         check_kday_table()
         hdata_kday.delete_data_from_hdata(
                 start_date=nowdate.strftime("%Y-%m-%d"),
                 end_date=nowdate.strftime("%Y-%m-%d")
                 )
-        hdata_kday.copy_from_stringio(df)
+        try:
+            hdata_kday.copy_from_stringio(kday_df)
+        except Exception as e:
+            my_dbg(f"error:{e}")
+            
+        
     else:
         my_dbg('#ERROR kday_df is null')
 
